6b0944401ff11c7e9362513f2716da6c054eb1e8
hmkavut <63113851+hmkavut@users.noreply.github.com>
2025-07-16 19:12:47 +0100

    Alarm Clock is completed

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,35 @@
-function setAlarm() {}
+let intervalId;
+
+function setAlarm() {
+  const inputArea = document.getElementById("alarmSet");
+  let remaining = Number(inputArea.value);
+
+  if (isNaN(remaining) || remaining <= 0) return;
+
+  const countDown = document.getElementById("timeRemaining");
+
+  clearInterval(intervalId); // clear any previous timers
+  updateDisplay(remaining, countDown);
+
+  intervalId = setInterval(() => {
+    remaining--;
+
+    updateDisplay(remaining, countDown);
+
+    if (remaining <= 0) {
+      clearInterval(intervalId);
+      playAlarm();
+    }
+  }, 1000);
+}
+
+function updateDisplay(seconds, element) {
+  const mins = Math.floor(seconds / 60)
+    .toString()
+    .padStart(2, "0");
+  const secs = (seconds % 60).toString().padStart(2, "0");
+  element.innerText = `Time Remaining: ${mins}:${secs}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
